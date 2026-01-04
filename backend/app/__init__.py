a13cbeb220354a8f06999b45ab90e4e241f2566f
medbbh <mbabah3450@gmail.com>
2026-01-04 12:39:15 +0000

    Set Up flask

---
+++ backend/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+from .config import Config
+from .extensions import db, migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes.health import health_bp
+    app.register_blueprint(health_bp)
+
+    return app
